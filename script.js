e6e916bb5c1e05a44b743138336130a7b9362264
Ludusua <157521040+Ludusua@users.noreply.github.com>
2024-04-06 18:24:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// Задание 1
+// const age = prompt("Hello user, how old are you?")
+// console.log(+age * 365);
+
+
+// Задание 2
+// const num1 = prompt("Enter number1");
+// const num2 = prompt("Enter number2");
+// console.log(+num1 + +num2);
+
+// Задание 3
+// const num = +prompt("Введите любое число:");
+// if (num === 0)
+// {
+//     console.log("Вы ввели число 0")
+
+// }
+// else if (num % 2 === 0)
+// {
+//     console.log("Вы ввели четное число")
+    
+// }
+// else
+// {
+//     console.log("Вы ввели нечетное число!")
+// }
+
+
+// Задание 4
+const num = +prompt("Введите любое число:");
+
+ if (num % 1 === 0)
+{
+    console.log("Это значение можно преобразовать в число")
+    
+}
+else
+{
+    console.log("Это значение преобразовать в число не получится. Получится NaN")
+}
